053d5c823741cbd5a40568c6b5d71fe08b6bd8fe
Robert Call <robertcall94@gmail.com>
2019-03-12 23:22:02 +0300

    Add project files.

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Square = new System.Windows.Forms.Panel();
+            this.Start_button = new System.Windows.Forms.Button();
+            this.Snake_step_timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Square
+            // 
+            this.Square.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Square.BackColor = System.Drawing.Color.Gold;
+            this.Square.Location = new System.Drawing.Point(12, 61);
+            this.Square.Name = "Square";
+            this.Square.Size = new System.Drawing.Size(800, 500);
+            this.Square.TabIndex = 0;
+            // 
+            // Start_button
+            // 
+            this.Start_button.Location = new System.Drawing.Point(737, 23);
+            this.Start_button.Name = "Start_button";
+            this.Start_button.Size = new System.Drawing.Size(75, 23);
+            this.Start_button.TabIndex = 1;
+            this.Start_button.Text = "Start";
+            this.Start_button.UseVisualStyleBackColor = true;
+            this.Start_button.Click += new System.EventHandler(this.Start_button_Click);
+            // 
+            // Snake_step_timer
+            // 
+            this.Snake_step_timer.Interval = 200;
+            this.Snake_step_timer.Tick += new System.EventHandler(this.Snake_step_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(370, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 573);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Start_button);
+            this.Controls.Add(this.Square);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Square;
+        private System.Windows.Forms.Button Start_button;
+        private System.Windows.Forms.Timer Snake_step_timer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        Label Head;
+        Label apple;
+        Label[] Body = new Label[159];
+        int x = 0, y = 0;
+        int length = 0;
+        Point Popka = new Point(1,1);
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_button_Click(object sender, EventArgs e)
+        {
+            Square.Controls.Clear();
+
+            Head = new Label();
+            Head.AutoSize = false;
+            Head.BackColor = Color.Green;
+            Head.Width = Head.Height = 50;
+            Head.Text = "";
+            Head.Location = new Point(0, 0);
+
+            apple = new Label();
+            apple.AutoSize = false;
+            apple.BackColor = Color.Red;
+            apple.Height = apple.Width = 50;
+            apple.Text = "";
+            apple.Location = new Point(rand.Next(16) * 50, rand.Next(10) * 50);
+            while(apple.Location == new Point(0,0))
+                apple.Location = new Point(rand.Next(16) * 50, rand.Next(10) * 50);
+
+            Square.Controls.Add(Head);
+            Square.Controls.Add(apple);
+            Snake_step_timer.Enabled = true;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch(keyData)
+            {
+                case Keys.Up: x = 0; y = -1; ; break;
+                case Keys.Down: x = 0; y = 1; ; break;
+                case Keys.Right: x = 1; y = 0; break;
+                case Keys.Left: x = -1; y = 0; break;
+                default: return base.ProcessCmdKey(ref msg, keyData);
+            }
+            return false;
+        }
+
+        private void Snake_step_Tick(object sender, EventArgs e)
+        {
+            Snake_step();
+            if (ItHead(apple.Location)) eat_apple();
+        }
+
+        private void Snake_step()
+        {
+            if (length != 0)
+            {
+                Popka = Body[length - 1].Location;
+                for (int i = length - 1; i > 0; i--)
+                    Body[i].Location = Body[i - 1].Location;
+                Body[0].Location = Head.Location;
+            }
+            else Popka = Head.Location;
+
+            Head.Location = new Point(Head.Location.X + x * 50, Head.Location.Y + y * 50);
+            if (Head.Location.X == 800) Head.Location = new Point(0, Head.Location.Y);
+            if (Head.Location.Y == 500) Head.Location = new Point(Head.Location.X, 0);
+            if (Head.Location.X < 0) Head.Location = new Point(750, Head.Location.Y);
+            if (Head.Location.Y < 0) Head.Location = new Point(Head.Location.X, 450);
+            
+            if(ItBody(Head.Location))
+            {
+                Snake_step_timer.Enabled = false;
+                if (length == 159)
+                    MessageBox.Show("You win!!");
+                else
+                    MessageBox.Show("You fail");
+            }
+        }
+
+        private void eat_apple()
+        {
+            while(ItHead(apple.Location)||ItBody(apple.Location))
+                apple.Location = new Point(rand.Next(16) * 50, rand.Next(10) * 50);
+
+            Body[length] = new Label();
+            Body[length].AutoSize = false;
+            Body[length].BackColor = Color.LightGreen;
+            Body[length].Text = "";
+            Body[length].Width = Body[length].Height = 50;
+            Body[length].Location = Popka;
+            Square.Controls.Add(Body[length]);
+            length++;
+        }
+
+        private bool ItHead(Point p)
+        {
+            if (p == Head.Location) return true;
+            return false;
+        }
+
+        private bool ItBody(Point p)
+        {
+            for (int i = 0; i < length; i++)
+                if (p == Body[i].Location) return true;
+            return false;
+        }
+    }
+}
